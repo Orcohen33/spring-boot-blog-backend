d49b8b2fa257299b628bdcdd6bb2e10f636d4c24
Orcohen33 <orcohen3322@gmail.com>
2022-10-22 16:09:19 +0200

    initial files

---
+++ src/main/java/com/orcohen/blogrestapi/entity/Post.java
@@ -0,0 +1,32 @@
+package com.orcohen.blogrestapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "posts",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String content;
+
+
+}
---
+++ src/main/java/com/orcohen/blogrestapi/repository/PostRepository.java
@@ -0,0 +1,4 @@
+package com.orcohen.blogrestapi.repository;
+
+public class PostRepository {
+}
